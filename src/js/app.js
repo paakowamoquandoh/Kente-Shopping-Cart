85e5cf34f127e7439797a87363b53261cc2d606c
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-15 23:21:04 +0000

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,8 @@ durationButton.addEventListener("click", ()=>{
 
 let newUploads = document.getElementById("newest");
 let allProducts = document.getElementById("allProducts");
-let lowPrice = document.getElementById("lowPrice");
-let trendy = document.getElementById("trendy");
+let lowestPrice = document.getElementById("lowPrice");
+let highestPrice = document.getElementById("trendy");
 
 let url = "mainProducts.json";
 let productsArea = document.getElementsByClassName("mainProArea")[0];
@@ -16,8 +16,8 @@ let productsArea = document.getElementsByClassName("mainProArea")[0];
 fetch(url).then((Response => Response.json())).then((data) => {
     const allProductsArray = [...data];
     const newProductsArray = [...data].splice(8, 12);
-    const lowPriceProductsArray = [...data];
-    const trendy_array = [...data];
+    const lowestPriceProductsArray = [...data];
+    const highestPriceArray = [...data];
 
     data.forEach((item, index) => {
         const {title, price, description, category} = item.fields;
@@ -123,17 +123,58 @@ fetch(url).then((Response => Response.json())).then((data) => {
         
     });
 
-    lowPrice.addEventListener("click", () => {
+    lowestPrice.addEventListener("click", () => {
         productsArea.innerHTML = "";
         durationButton.innerHTML = `
-        <h5>Sort By: All Products</h5>
+        <h5>Sort By: Lowest Prices</h5>
         <ion-icon name="chevron-down-outline"></ion-icon>
         `;
         durationOptions.classList.toggle("sortOptions_active");
 
-        lowPriceProductsArray.sort(({price:a}, {price:b}) => a-b);
+        lowestPriceProductsArray.sort((a, b) => a.fields.price - b.fields.price);
         
-        lowPriceProductsArray.forEach((item, index) => {
+        lowestPriceProductsArray.forEach((item, index) => {
+            const {title, price, description, category} = item.fields;
+            const {id} = item.sys;
+            const image = item.fields.image.fields.file.url;
+         
+            let card = document.createElement("a");
+            card.classList.add("itemCard")
+            card.innerHTML = `
+            <img src=${image} alt="">
+          <h5 class="cardTitle" title="African Print Dress">${title}</h5>
+          <p>${description}</p>
+          <div class="itemPrice">
+              <h5>$${price}</h5>
+          </div>
+          <div class="colorTag">
+          <div class="stars">
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+          <ion-icon name="star"></ion-icon>
+        </div>
+              <button class="proCart" data-id = ${id}>Buy</button>
+          </div>
+            `;
+            productsArea.appendChild(card);
+        });
+        
+    })
+
+
+    highestPrice.addEventListener("click", () => {
+        productsArea.innerHTML = "";
+        durationButton.innerHTML = `
+        <h5>Sort By: Highest Prices</h5>
+        <ion-icon name="chevron-down-outline"></ion-icon>
+        `;
+        durationOptions.classList.toggle("sortOptions_active");
+
+        highestPriceProductsArray.sort((a, b) => b.fields.price - a.fields.price);
+        
+        highestPriceArray.forEach((item, index) => {
             const {title, price, description, category} = item.fields;
             const {id} = item.sys;
             const image = item.fields.image.fields.file.url;
