d7aed2663f212eb178bb1a32c3b712ff794231d1
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-15 20:11:06 +0000

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,8 @@ let durationOptions = document.getElementsByClassName("sortOptions")[0];
 durationButton.addEventListener("click", ()=>{
     durationOptions.classList.toggle("sortOptions_active")
 })
+
+let newUploads = document.getElementById("newest");
+let allProducts = document.getElementById("allProducts");
+let lowPrice = document.getElementById("lowPrice");
+let trendy = document.getElementById("trendy");
